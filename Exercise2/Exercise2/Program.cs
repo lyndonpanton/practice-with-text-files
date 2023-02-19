c2de626e72c7ecec993562e6311dc6a8239f1c50
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-19 00:36:06 +0000

    Read the contents of the file

--- Exercise2/Exercise2/Program.cs
+++ Exercise2/Exercise2/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Exercise2
+﻿
+namespace Exercise2
 {
+    using System.IO;
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // initialise stream
+            StreamReader fileInput = null;
+
+            try
+            {
+                // open file
+                fileInput = File.OpenText("practice_with_text_files.txt");
+
+                // read first line
+                string line = fileInput.ReadLine();
+
+                // loop while more lines remain
+                while (line != null)
+                {
+                    // output the current line
+                    Console.WriteLine(line);
+                    // get the next line
+                    line = fileInput.ReadLine();
+                }
+            } catch (Exception exception)
+            {
+                Console.WriteLine("An error occured");
+            } finally
+            {
+                // if there is nothing left to be read
+                if (fileInput != null)
+                {
+                    // close the  file
+                    fileInput.Close();
+                }
+            }
         }
     }
 }
