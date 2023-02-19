3215a720957802d674f0af59cfd2b4926ab96ee1
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-19 00:45:11 +0000

    Created and wrote the contents of a file
    Completed problem 3

--- Exercise2/Exercise2/Program.cs
+++ Exercise2/Exercise2/Program.cs
@@ -2,19 +2,20 @@
 namespace Exercise2
 {
     using System.IO;
+    using System.Collections.Generic;
     internal class Program
     {
         static void Main(string[] args)
         {
-            // initialise stream
+            // initialise the file stream
             StreamReader fileInput = null;
 
             try
             {
-                // open file
+                // open the file
                 fileInput = File.OpenText("practice_with_text_files.txt");
 
-                // read first line
+                // read the first line
                 string line = fileInput.ReadLine();
 
                 // loop while more lines remain
@@ -27,7 +28,8 @@ namespace Exercise2
                 }
             } catch (Exception exception)
             {
-                Console.WriteLine("An error occured");
+                // catch error if one occurred
+                Console.WriteLine("An error occurred");
             } finally
             {
                 // if there is nothing left to be read
@@ -35,6 +37,37 @@ namespace Exercise2
                 {
                     // close the  file
                     fileInput.Close();
+                }
+            }
+
+            // initialise the file stream
+            StreamWriter fileOutput = null;
+
+            try
+            {
+                // create the file
+                fileOutput = File.CreateText("more_practice_with_text_files.txt");
+
+                // write the first line of the file
+                fileOutput.WriteLine("This is a text file");
+
+                // write the second line of the file, etc.
+                fileOutput.WriteLine("This text file was created on: 19/02/2023");
+                fileOutput.WriteLine("This text file contains characters");
+                fileOutput.WriteLine();
+                fileOutput.WriteLine("This text file was created using the C# StreamWriter class");
+                fileOutput.WriteLine("This text file was created by Lyndon Panton");
+            } catch (Exception exception)
+            {
+                // catch error if one occurred
+                Console.WriteLine("An error occurred");
+            } finally
+            {
+                // if there is nothing left to be written
+                if (fileOutput != null)
+                {
+                    // close the file
+                    fileOutput.Close();
                 }
             }
         }
